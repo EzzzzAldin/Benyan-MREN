c0daef6526211c9299a16c25a4418829e5e1b803
Ezz Aldin <zezomomhamed74@gmail.com>
2026-04-25 03:01:42 +0300

    Lecture 1

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Dashboard/Navbar/Navbar";
+
+function Dashboard() {
+  return (
+    <>
+      <Navbar />
+      <h1>Test Content</h1>
+    </>
+  );
+}
+
+export default Dashboard;
